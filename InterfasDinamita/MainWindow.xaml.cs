81982545493daf5b403577b71310e696d26c1e8f
alumno <alumno@E321>
2019-10-03 08:24:55 -0700

    dfgdfgdfgdfgd

--- InterfasDinamita/MainWindow.xaml.cs
+++ InterfasDinamita/MainWindow.xaml.cs
@@ -36,17 +36,45 @@ namespace InterfasDinamita
                 case 1:
                     gridInformacion.Children.Add(new Docente());
                     break;
+
+
             }
+
+            switch(cbUsuario.SelectedIndex)
+            {
+                case 0:
+                      ((Alumno)(gridInformacion.Children[0])).txbNombreA.Text="";
+                      ((Alumno)(gridInformacion.Children[0])).txbMatriculaA.Text = "";
+                      ((Alumno)(gridInformacion.Children[0])).txbPreparatoriaA.Text = "";
+                    break;
+
+                case 1:
+                    ((Docente)(gridInformacion.Children[0])).txbNombreD.Text = "";
+                    ((Docente)(gridInformacion.Children[0])).txbCodigoD.Text = "";
+                    ((Docente)(gridInformacion.Children[0])).txbTituloD.Text = "";
+                    ((Docente)(gridInformacion.Children[0])).txbMateriaD.Text = "";
+                    break;
+
+                default:
+                    break;
+            }
+            btnGuardar.Visibility = Visibility.Visible;
+            btnCancelar.Visibility = Visibility.Visible;
+
         }
 
         private void BtnGuardar_Click(object sender, RoutedEventArgs e)
         {
             gridInformacion.Children.Clear();
+            btnGuardar.Visibility = Visibility.Hidden;
+            btnCancelar.Visibility = Visibility.Hidden;
         }
 
         private void BtnCancelar_Click(object sender, RoutedEventArgs e)
         {
             gridInformacion.Children.Clear();
+            btnCancelar.Visibility = Visibility.Hidden;
+            btnGuardar.Visibility = Visibility.Hidden;
         }
 
 
